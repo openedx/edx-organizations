e23865874561ff09e9c59d281b74b132e248f702
Clinton Blackburn <cblackburn@edx.org>
2016-05-11 22:10:07 -0400

    Replaced JSONWebTokenAuthentication with edX's own JwtAuthentication
    This module knows how to properly parse our JWT claims.
    
    ECOM-4414

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
         'django>=1.8,<1.9',
         'django-model-utils>=1.4.0,<1.5.0',
         'djangorestframework>=3.2.0,<3.4.0',
-        'djangorestframework-jwt>=1.6.0,<=1.7.2',
         'edx-opaque-keys>=0.1.2,<1.0.0',
         'djangorestframework-oauth>=1.1.0,<2.0.0',
         'edx-django-oauth2-provider>=0.5.0,<1.0.0',
+        'edx-drf-extensions>=0.5.1,<1.0.0',
     ],
 )
