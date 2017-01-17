df68a277bfb462e8be107375606e930055ed7713
Clinton Blackburn <cblackburn@edx.org>
2017-01-17 15:06:07 -0500

    Updated package requirements
    - Alphabetized requirements
    - Added Pillow since it is needed to use ImageField
    - Relaxed django-model-utils requirement
    
    ECOM-6862

--- setup.py
+++ setup.py
@@ -24,11 +24,12 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'django>=1.8,<1.9',
-        'django-model-utils>=1.4.0,<1.5.0',
+        'django-model-utils>=1.4.0',
         'djangorestframework>=3.2.0,<3.4.0',
-        'edx-opaque-keys>=0.1.2,<1.0.0',
         'djangorestframework-oauth>=1.1.0,<2.0.0',
         'edx-django-oauth2-provider>=0.5.0,<1.0.0',
         'edx-drf-extensions>=0.5.1,<1.0.0',
+        'edx-opaque-keys>=0.1.2,<1.0.0',
+        'Pillow',
     ],
 )
