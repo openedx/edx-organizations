8a4862978318657db38b73f05623bf7bc7c1626b
Douglas Hall <douglashall@dhcp-18-189-92-69.dyn.MIT.EDU>
2017-08-18 15:53:49 -0400

    Fix get_organization bug.
    This fixes a bug which was causing the get_organization function
    to return the same organization every time. The issue was that we
    were not using the given organization ID to filter the Django
    queryset.
    
    ENT-585

--- organizations/tests/test_data.py
+++ organizations/tests/test_data.py
@@ -6,6 +6,8 @@ Note: 'Unit Test: ' labels are output to the console during test runs
 import organizations.data as data
 import organizations.tests.utils as utils
 
+from organizations.tests.factories import OrganizationFactory
+
 
 class OrganizationsDataTestCase(utils.OrganizationsTestCaseBase):
     """
@@ -22,3 +24,11 @@ class OrganizationsDataTestCase(utils.OrganizationsTestCaseBase):
                 'description': 'Local Organization Description'
             })
         self.assertGreater(organization['id'], 0)
+
+    def test_fetch_organization(self):
+        """ Unit Test: test_fetch_organization"""
+        organization1 = OrganizationFactory.create()
+        organization2 = OrganizationFactory.create()
+        with self.assertNumQueries(2):
+            self.assertEqual(data.fetch_organization(organization1.id)['id'], organization1.id)
+            self.assertEqual(data.fetch_organization(organization2.id)['id'], organization2.id)
