1a447a36fbf88f1b617ce4d10a865a5101a4b647
rabiaiftikhar <rabia.iftikhar@arbisoft.com>
2017-11-16 18:16:32 +0500

    EDUCATOR-1672 validate org short_name field and add help_text in django admin to prohibit unicode

---
+++ organizations/migrations/0005_auto_20171116_0640.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('organizations', '0004_auto_20170413_2315'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='organization',
+            name='short_name',
+            field=models.CharField(help_text="Please do not use any spaces or special characters in short name. This short name will be used in the course's course key.", max_length=255, verbose_name=b'Short Name', db_index=True),
+        ),
+    ]
