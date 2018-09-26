756840d614a0c6756ab2c78df9c2955fb4321d3f
John Eskew <jeskew@edx.org>
2018-09-26 10:02:15 -0400

    Fix Django20 deprecation warning - add app_name/remove namespace

--- organizations/urls.py
+++ organizations/urls.py
@@ -3,7 +3,7 @@ URLS for organizations
 """
 from django.conf.urls import url, include
 
-# pylint: disable=invalid-name
+app_name = 'organizations'  # pylint: disable=invalid-name
 urlpatterns = [
-    url(r'^v0/', include('organizations.v0.urls', namespace='v0')),
+    url(r'^v0/', include('organizations.v0.urls')),
 ]
