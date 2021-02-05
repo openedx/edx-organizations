86de5e24e639fceb638a63d057ad0997d22c684a
Kyle McCormick <kmccormick@edx.org>
2021-02-05 16:52:12 -0500

    feat: add (de)activate admin actions for OrganizationCourse

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,21 +1,24 @@
 """ Django admin pages for organization models """
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.translation import gettext_lazy as _
 
 from organizations.models import Organization, OrganizationCourse
 
 
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    """ Admin for the Organization model. """
-    actions = ['activate_selected', 'deactivate_selected']
-    list_display = ('name', 'short_name', 'logo', 'active',)
-    list_filter = ('active',)
-    ordering = ('name', 'short_name',)
-    readonly_fields = ('created',)
-    search_fields = ('name', 'short_name',)
+class ActivateDeactivateAdminMixin:
+    """
+    Provides the activate_selected and deactivate_select bulk actions.
+
+    Hides the delete_selected actions; we'd much rows are deactivated than
+    deleted.
+    """
+
+    HISTORY_DISCLAIMER = _(
+        "Please note: as a bulk action, this will not be reflected in the model's history table."
+    )
 
     def get_actions(self, request):
+        """ Return set of Django admin actions, removing the delete action """
         actions = super().get_actions(request)
 
         # Remove the delete action.
@@ -26,38 +29,53 @@ class OrganizationAdmin(admin.ModelAdmin):
 
     def activate_selected(self, request, queryset):
         """ Activate the selected entries. """
-        queryset.update(active=True)
         count = queryset.count()
+        queryset.update(active=True)
+        model_name = self.__class__.__name__
 
         if count == 1:
-            message = _('1 organization entry was successfully activated')
+            message = _('1 {model_name} entry was successfully activated.')
         else:
-            message = _('{count} organization entries were successfully activated')
-            message.format(count=count)  # pylint: disable=no-member
-
+            message = _('{count} {model_name} entries were successfully activated.')
+        message = message.format(count=count, model_name=model_name)
         self.message_user(request, message)
+        self.message_user(request, self.HISTORY_DISCLAIMER, level=messages.WARNING)
 
     def deactivate_selected(self, request, queryset):
         """ Deactivate the selected entries. """
-        queryset.update(active=False)
         count = queryset.count()
+        queryset.update(active=False)
+        model_name = self.__class__.__name__
 
         if count == 1:
-            message = _('1 organization entry was successfully deactivated')
+            message = _('1 {model_name} entry was successfully deactivated.')
         else:
-            message = _('{count} organization entries were successfully deactivated')
-            message.format(count=count)  # pylint: disable=no-member
-
+            message = _('{count} {model_name} entries were successfully deactivated.')
+        message = message.format(count=count, model_name=model_name)
         self.message_user(request, message)
+        self.message_user(request, self.HISTORY_DISCLAIMER, level=messages.WARNING)
 
     deactivate_selected.short_description = _('Deactivate selected entries')
     activate_selected.short_description = _('Activate selected entries')
 
 
+@admin.register(Organization)
+class OrganizationAdmin(ActivateDeactivateAdminMixin, admin.ModelAdmin):
+    """ Admin for the Organization model. """
+    actions = ['activate_selected', 'deactivate_selected']
+    list_display = ('name', 'short_name', 'logo', 'active',)
+    list_filter = ('active',)
+    ordering = ('name', 'short_name',)
+    readonly_fields = ('created',)
+    search_fields = ('name', 'short_name',)
+
+
 @admin.register(OrganizationCourse)
-class OrganizationCourseAdmin(admin.ModelAdmin):
-    """ Admin for the CourseOrganization model. """
+class OrganizationCourseAdmin(ActivateDeactivateAdminMixin, admin.ModelAdmin):
+    """ Admin for the OrganizationCourse model. """
+    actions = ['activate_selected', 'deactivate_selected']
     list_display = ('course_id', 'organization', 'active')
+    list_filter = ('active',)
     ordering = ('course_id', 'organization__name',)
     search_fields = ('course_id', 'organization__name', 'organization__short_name',)
 
