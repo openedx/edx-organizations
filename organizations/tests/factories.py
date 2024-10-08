d7a5b6ea512a7ed9d88adb911c167f33cea2199b
klu2300031947 <163754507+klu2300031947@users.noreply.github.com>
2024-10-08 21:26:32 +0530

    Update factories.py

--- organizations/tests/factories.py
+++ organizations/tests/factories.py
@@ -1,36 +1,11 @@
-"""
-Provides factory for User.
-"""
-from django.contrib.auth.models import User
-import factory
-from factory.django import DjangoModelFactory
+from factory import Factory, Faker
+from myapp.models import Product
 
-from organizations.models import Organization
-
-
-class UserFactory(DjangoModelFactory):
-    """ User creation factory."""
+class ProductFactory(Factory):
     class Meta:
-        model = User
-        django_get_or_create = ('email', 'username')
+        model = Product
 
-    username = factory.Sequence(lambda arg: f'robot{arg}')
-    email = factory.Sequence(lambda arg: f'robot+test+{arg}@edx.org')
-    password = factory.PostGenerationMethodCall('set_password', 'test')
-    first_name = factory.Sequence(lambda arg: f'Robot{arg}')
-    last_name = 'Test'
-    is_staff = False
-    is_active = True
-    is_superuser = False
-
-
-class OrganizationFactory(DjangoModelFactory):
-    """ Organization creation factory."""
-    class Meta:
-        model = Organization
-
-    name = factory.Sequence(lambda arg: f'organization name {arg}')
-    short_name = factory.Sequence(lambda arg: f'name{arg}')
-    description = factory.Sequence(lambda arg: f'description{arg}')
-    logo = None
-    active = True
+    name = Faker('word')
+    category = Faker('word')
+    price = Faker('random_number', digits=2)
+    available = Faker('boolean')
